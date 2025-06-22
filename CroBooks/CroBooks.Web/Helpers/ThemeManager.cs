05f3fba5dad5a1174ce47ce465bd2f1a96a749ec
Fulmetal <itesanov@gmail.com>
2025-06-22 17:36:50 +0200

    client list and package updates

---
+++ CroBooks/CroBooks.Web/Helpers/ThemeManager.cs
@@ -0,0 +1,40 @@
+﻿using MudBlazor;
+
+namespace CroBooks.Web.Helpers
+{
+    public static class ThemeManager
+    {
+        public static MudTheme DefaultTheme()
+        {
+            //var primary = Colors.Blue.Default;
+            //var onPrimary = Colors.Shades.White;
+            //var primaryContainer = 
+
+
+            MudTheme defaultTheme = new MudTheme()
+            {
+                PaletteLight = new PaletteLight()
+                {
+                    Primary = Colors.Blue.Default,
+                    PrimaryLighten = Colors.Blue.Lighten4,
+                    Secondary = Colors.Blue.Lighten4,
+                    
+                    AppbarBackground = Colors.Red.Default,
+                },
+                PaletteDark = new PaletteDark()
+                {
+                    Primary = Colors.Blue.Lighten1
+                },
+
+                LayoutProperties = new LayoutProperties()
+                {
+                    DrawerWidthLeft = "260px",
+                    DrawerWidthRight = "300px"
+                }
+            };
+
+            return defaultTheme;
+        }
+
+    }
+}
